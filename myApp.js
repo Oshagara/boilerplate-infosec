453f846e3b43b4da13b384fa1c6a00eb98990220
Israel Oluwapelumi Oshagara <60827839+Oshagara@users.noreply.github.com>
2024-08-06 13:46:28 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
+
+/*It can help to provide an extra layer of security to reduce the ability of attackers to determine the software that a server uses, known as “fingerprinting.” 
+Though not a security issue itself, reducing the ability to fingerprint an application improves its overall security posture. Server software can be fingerprinted 
+by quirks in how it responds to specific requests, for example in the HTTP response headers.*/
+
 app.use(helmet.hidePoweredBy());
+
+
+/*Block ClickJacking--------For example, imagine an attacker who builds a web site that has a button on it that says “click here for a free iPod”. However, on top of that web page, 
+the attacker has loaded an iframe with your mail account, and lined up exactly the “delete all messages” button directly on top of the “free iPod” button. 
+The victim tries to click on the “free iPod” button but instead actually clicked on the invisible “delete all messages” button. In essence, the attacker has 
+“hijacked” the user’s click, hence the name “Clickjacking”.*/
 
 app.use(helmet.frameguard({action: 'deny'}));
 
